986f6e6847c25fd22159b09c5496dc5046848655
KumarsantoshPRO <kumar.santosh@protivitglobal.in>
2025-06-12 09:11:26 +0530

    Budget Master first development completed

--- budgetmaster/webapp/controller/View1.controller.js
+++ budgetmaster/webapp/controller/View1.controller.js
@@ -7,11 +7,42 @@ sap.ui.define([
     return Controller.extend("pgp.com.budgetmaster.controller.View1", {
         onInit() {
             sap.ui.core.UIComponent.getRouterFor(this);
-            this.getOwnerComponent()
-                .getRouter()
-                .attachRoutePatternMatched(this._onRouteMatched, this);
+
+            this.readCall();
+            this.definations();
         },
-        _onRouteMatched: function (oEvent) {
+        definations: function () {
+            var oFilters = {
+                PRJ_TYPE: "",
+                ZTLIST: "",
+                ZCATEG: ""
+            };
+            this.getView().setModel(new JSONModel(oFilters), "oModelFilter");
+        },
+        onSearch: function () {
+            var oTable = this.getView().byId("table");
+            var oBinding = oTable.getBinding("rows");
+            var filterProject = this.getView().getModel("oModelFilter").getProperty("/PRJ_TYPE");
+            var filterTech = this.getView().getModel("oModelFilter").getProperty("/ZTLIST");
+            var filterCat = this.getView().getModel("oModelFilter").getProperty("/ZCATEG");
+            var aFiltersList = []
+            if (filterProject) {
+                aFiltersList.push(new sap.ui.model.Filter("PRJ_TYPE", sap.ui.model.FilterOperator.EQ, filterProject));
+            }
+            if (filterTech) {
+                aFiltersList.push(new sap.ui.model.Filter("ZTLIST", sap.ui.model.FilterOperator.EQ, filterTech));
+            }
+            if (filterCat) {
+                aFiltersList.push(new sap.ui.model.Filter("ZCATEG", sap.ui.model.FilterOperator.EQ, filterCat));
+            }
+
+            var aFilters = new sap.ui.model.Filter(aFiltersList, true); // true for AND
+            oBinding.filter(aFilters);
+        },
+        onFilterBarClear: function () {
+            this.getView().getModel("oModelFilter").setProperty("/PRJ_TYPE", "");
+            this.getView().getModel("oModelFilter").setProperty("/ZTLIST", "");
+            this.getView().getModel("oModelFilter").setProperty("/ZCATEG", "");
             this.readCall();
         },
         onAddNew: function () {
@@ -20,15 +51,52 @@ sap.ui.define([
                 nav: "null",
             });
         },
+        onClickofItem: function (oEvent) {
+            var PRJ_TYPE = oEvent.getParameter("rowContext").getObject().PRJ_TYPE;
+            var ZINITI = oEvent.getParameter("rowContext").getObject().ZINITI;
+            var ZTLIST = oEvent.getParameter("rowContext").getObject().ZTLIST;
+            var ZCATEG = oEvent.getParameter("rowContext").getObject().ZCATEG;
+            var ZEXPTP = oEvent.getParameter("rowContext").getObject().ZEXPTP;
+            var ZCAPEX_OPEX = oEvent.getParameter("rowContext").getObject().ZCAPEX_OPEX;
+            var WERKS = oEvent.getParameter("rowContext").getObject().WERKS;
+            var ZBUDG_CODE = oEvent.getParameter("rowContext").getObject().ZBUDG_CODE;
+            var nav = "(MANDT='',PRJ_TYPE='" + PRJ_TYPE + "',ZINITI='" + ZINITI + "',ZTLIST='" + ZTLIST + "',ZCATEG='" + ZCATEG + "',ZEXPTP='" + ZEXPTP + "',ZCAPEX_OPEX='" + ZCAPEX_OPEX + "',WERKS='" + WERKS + "',ZBUDG_CODE='" + ZBUDG_CODE + "')"
+            this.oRouter = this.getOwnerComponent().getRouter();
+            this.oRouter.navTo("View2", {
+                nav: nav,
+            });
+        },
         readCall: function () {
-            var sService = "/sap/opu/odata/sap/ZBUDGET_MASTER_SRV";
-            var oModel = new sap.ui.model.odata.ODataModel(sService, true);
+            var oModel = this.getOwnerComponent().getModel();
             var sPath = "/et_budget_masterSet"
             this.getView().setBusy(true);
             oModel.read(sPath, {
                 success: function (data) {
-                    this.getView().setModel(new JSONModel(data), "ModelForTable");
-                    this.getView().setBusy(false);
+                    oModel.read("/ZpsBudPerHodSet", {
+                        success: function (aData) {
+                            for (var j = 0; j < data.results.length; j++) {
+                                var aTableElement = data.results[j];
+                                for (let index = 0; index < aData.results.length; index++) {
+                                    const element = aData.results[index];
+
+                                    if (element.Bname === aTableElement.ZPRJ_HOD) {
+                                        aTableElement.ZPRJ_HOD = element.NameTextc + "-" + aTableElement.ZPRJ_HOD
+                                        // this.getView().getModel("oModelTempValue").setProperty("/HODName", element.NameTextc + "-");
+                                    }
+                                    if (element.Bname === aTableElement.ZWBS_OWNER) {
+                                        aTableElement.ZWBS_OWNER = element.NameTextc + "-" + aTableElement.ZWBS_OWNER
+                                        // this.getView().getModel("oModelTempValue").setProperty("/ResName", element.NameTextc + "-");
+                                    }
+
+                                }
+                            }
+
+                            this.getView().setBusy(false);
+                            this.getView().setModel(new JSONModel(data.results), "ModelForTable");
+                        }.bind(this),
+                        error: function () { }.bind(this)
+                    });
+
                 }.bind(this),
                 error: function (sError) {
                     this.getView().setBusy(false);
--- budgetmaster/webapp/controller/View2.controller.js
+++ budgetmaster/webapp/controller/View2.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageBox",
 ], function (
-	Controller, JSONModel
+	Controller, JSONModel, Filter, FilterOperator, MessageBox
 ) {
 	"use strict";
 
@@ -12,9 +15,66 @@ sap.ui.define([
 			this.getOwnerComponent()
 				.getRouter()
 				.attachRoutePatternMatched(this._onRouteMatched, this);
+			this.definations();
+		},
+
+		onBeforeRendering() {
+			this.getView().setModel(new JSONModel({}), "oModel");
+		},
+		definations: function () {
+			var properties = {
+				keyField: false,
+				editClicked: false,
+				saveButton: false,
+				updateButton: false,
+				cancelButton: false,
+				editButton: false,
+				copyButton: false,
+				deleteButton: false,
+				cancelButton: false
+			};
+			this.getView().setModel(new JSONModel(properties), "oPropertyModel");
+			var tempValue = {
+				HODName: "",
+				ResName: ""
+			}
+			this.getView().setModel(new JSONModel(tempValue), "oModelTempValue");
 		},
 		_onRouteMatched: function (oEvent) {
-			this.createLocalModel();
+			var nav = oEvent.getParameter("arguments").nav
+			this.nav = nav;
+			this.screenBehave(nav);
+
+		},
+		screenBehave: function (nav) {
+			if (nav === 'null') {
+				this.getView().setModel(new JSONModel({
+					HODName: "",
+					ResName: ""
+				}), "oModelTempValue");
+				this.getView().getModel("oPropertyModel").setProperty("/editClicked", true);
+				this.getView().getModel("oPropertyModel").setProperty("/keyField", true);
+				this.getView().getModel("oPropertyModel").setProperty("/saveButton", true);
+				this.getView().getModel("oPropertyModel").setProperty("/updateButton", false);
+
+				this.getView().getModel("oPropertyModel").setProperty("/editButton", false);
+				this.getView().getModel("oPropertyModel").setProperty("/copyButton", false);
+				this.getView().getModel("oPropertyModel").setProperty("/deleteButton", false);
+				this.getView().getModel("oPropertyModel").setProperty("/cancelButton", true);
+				this.createLocalModel();
+			} else {
+
+				this.getView().getModel("oPropertyModel").setProperty("/editClicked", false);
+				this.getView().getModel("oPropertyModel").setProperty("/keyField", false);
+				this.getView().getModel("oPropertyModel").setProperty("/saveButton", false);
+				this.getView().getModel("oPropertyModel").setProperty("/updateButton", false);
+
+				this.getView().getModel("oPropertyModel").setProperty("/editButton", true);
+				this.getView().getModel("oPropertyModel").setProperty("/copyButton", true);
+				this.getView().getModel("oPropertyModel").setProperty("/deleteButton", true);
+				this.getView().getModel("oPropertyModel").setProperty("/cancelButton", false);
+				this.readCall(nav);
+			}
 		},
 		createLocalModel: function () {
 			var oDataForModel = {
@@ -32,29 +92,284 @@ sap.ui.define([
 				"ZWBS_OWNER": "",
 				"ZPRJ_HOD": "",
 				"ZREMARKS": ""
-			}
+			};
 			this.getView().setModel(new JSONModel(oDataForModel), "oModel");
 		},
+		readCall: function (nav) {
+			var oModel = this.getOwnerComponent().getModel();
+			var sPath = "/et_budget_masterSet" + nav;
+			this.getView().setBusy(true);
+
+			oModel.read(sPath, {
+				success: function (data) {
+
+					oModel.read("/ZpsBudPerHodSet", {
+						success: function (aData) {
+
+							for (let index = 0; index < aData.results.length; index++) {
+								const element = aData.results[index];
+								if (element.Bname === data.ZPRJ_HOD) {
+									this.getView().getModel("oModelTempValue").setProperty("/HODName", element.NameTextc + "-");
+								}
+								if (element.Bname === data.ZWBS_OWNER) {
+									this.getView().getModel("oModelTempValue").setProperty("/ResName", element.NameTextc + "-");
+								}
+
+							}
+							this.getView().setBusy(false);
+							this.getView().setModel(new JSONModel(data), "oModel");
+						}.bind(this),
+						error: function () { }.bind(this)
+					});
+
+
+				}.bind(this),
+				error: function (sError) {
+					this.getView().setBusy(false);
+				}.bind(this)
+			})
+		},
 		onSave: function () {
-			var sService = "/sap/opu/odata/sap/ZBUDGET_MASTER_SRV";
-			var oModel = new sap.ui.model.odata.ODataModel(sService, true);
+			var oModel = this.getOwnerComponent().getModel();
 			var sPath = "/et_budget_masterSet";
 			var payload = this.getView().getModel("oModel").getData();
+			payload.ZBUDG_CODE = '';
+			if (payload.ZBUDG_STATUS === 'false') {
+				payload.ZBUDG_STATUS = false;
+			} else {
+				payload.ZBUDG_STATUS = true;
+			}
 
-			// var sPath = "/et_budget_master_assignSet";
-			// var payload = {
-			// 	"MANDT": "240",
-			// 	"ZBUDG_CODE": "S1234",
-			// 	"ZBUDG_DISC": "S1234",
-			// 	"ZWBS_YEAR": "2024",
-			// 	"POSID": "SKP"
-			// };
-		 
+			this.getView().setBusy(true);
+			var that = this;
 			oModel.create(sPath, payload, {
-				success: function (oData, response) { }.bind(this),
-				error: function (sError) { }
+				success: function (oData, response) {
+					MessageBox.success("Entry created", {
+						actions: ["Ok"],
+						onClose: function (oAction) {
+							if (oAction === "Ok") {
+								that.screenBehave("null");
+							}
+						}
+					});
+					this.getView().setBusy(false);
+				}.bind(this),
+				error: function (sError) {
+					this.getView().setBusy(false);
+				}
 			})
-		}
+		},
+		onEditButtonPress: function () {
+			this.getView().getModel("oPropertyModel").setProperty("/editClicked", true);
+			this.getView().getModel("oPropertyModel").setProperty("/keyField", false);
+			this.getView().getModel("oPropertyModel").setProperty("/saveButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/updateButton", true);
+
+			this.getView().getModel("oPropertyModel").setProperty("/editButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/copyButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/deleteButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/cancelButton", true);
+		},
+		onUpdateButtonPress: function () {
+			var oModel = this.getOwnerComponent().getModel();
+			var sPath = "/et_budget_masterSet" + this.nav;
+			var payload = this.getView().getModel("oModel").getData();
+			this.getView().setBusy(true);
+			if (payload.ZBUDG_STATUS === 'false' || payload.ZBUDG_STATUS === false) {
+				payload.ZBUDG_STATUS = false;
+			} else if (payload.ZBUDG_STATUS === 'true' || payload.ZBUDG_STATUS === true) {
+				payload.ZBUDG_STATUS = true;
+			}
+			var that = this;
+			oModel.update(sPath, payload, {
+				success: function () {
+					MessageBox.success("Update completed", {
+						actions: ["Ok"],
+						onClose: function (oAction) {
+							if (oAction === "Ok") {
+								that.getView().setBusy(false);
+								that.screenBehave(that.nav);
+							}
+						}
+					});
+
+
+				}.bind(this),
+				error: function (sError) {
+					this.getView().setBusy(false);
+				}.bind(this)
+			})
+		},
+		onDeleteButtonPress: function () {
+
+			var oModel = this.getOwnerComponent().getModel();
+			var sPath = "/et_budget_masterSet" + this.nav;
+			var that = this;
+			MessageBox.warning("Are you sure to delete this?", {
+				actions: ["Yes", "Cancel"],
+				onClose: function (oAction) {
+
+					if (oAction === "Yes") {
+						that.getView().setBusy(true);
+						oModel.remove(sPath, {
+							success: function () {
+								MessageBox.success("Deletion completed", {
+									actions: ["Ok"],
+									onClose: function (oAction) {
+										if (oAction === "Ok") {
+											that.screenBehave("null");
+											that.getView().setBusy(false);
+										}
+									}
+								});
+
+							}.bind(this),
+							error: function (sError) {
+								that.screenBehave(that.nav);
+								that.getView().setBusy(false);
+							}.bind(this)
+						})
+					}
+				},
+			});
+
+		},
+		onCopyButtonPress: function () {
+			this.getView().getModel("oPropertyModel").setProperty("/editClicked", true);
+			this.getView().getModel("oPropertyModel").setProperty("/keyField", true);
+			this.getView().getModel("oPropertyModel").setProperty("/saveButton", true);
+			this.getView().getModel("oPropertyModel").setProperty("/updateButton", false);
+
+			this.getView().getModel("oPropertyModel").setProperty("/editButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/copyButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/deleteButton", false);
+			this.getView().getModel("oPropertyModel").setProperty("/cancelButton", true);
+		},
+		onCancelButtonPress: function () {
+			var that = this;
+			MessageBox.information("Changes not saved, Do you want to cancel?", {
+				actions: ["Yes", "Cancel"],
+				onClose: function (oAction) {
+
+					if (oAction === "Yes") {
+						that.screenBehave(that.nav);
+					}
+				},
+			});
+		},
+		// F4 calls
+		// HOD
+		// on Value Help(F4)
+		onHODValueHelp: function () {
+			if (!this.hodFrag) {
+				this.hodFrag = sap.ui.xmlfragment(
+					"pgp.com.budgetmaster.view.fragments.hodF4",
+					this
+				);
+				this.getView().addDependent(this.hodFrag);
+				this._hodTemp = sap.ui
+					.getCore()
+					.byId("idSLhodValueHelp")
+					.clone();
+			}
+
+			var aFilter = [];
+			sap.ui.getCore().byId("idSDhodF4").bindAggregation("items", {
+				path: "/ZpsBudPerHodSet",
+				filters: aFilter,
+				template: this._hodTemp,
+			});
+
+			this.hodFrag.open();
+		},
+
+		// on Value Help - Search/liveChange
+		onValueHelpSearch_hod: function (oEvent) {
+			var aFilter = [];
+			var sValue = oEvent.getParameter("value");
+			var sPath = "/ZpsBudPerHodSet";
+			var oSelectDialog = sap.ui.getCore().byId(oEvent.getParameter("id"));
+
+			var oFilterName = new Filter(
+				[new Filter("NameTextc", FilterOperator.Contains, sValue)],
+				false
+			);
+			aFilter.push(oFilterName);
+
+			oSelectDialog.bindAggregation("items", {
+				path: sPath,
+				filters: aFilter,
+				template: this.hodFrag,
+			});
+		},
+
+		// on Value Help - Confirm
+		onValueHelpConfirm_hod: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem"),
+				sSelectedValue = oSelectedItem.getProperty("title"),
+				hodName = oSelectedItem.getProperty("description") + "-";
+			this
+				.getView()
+				.getModel("oModel")
+				.setProperty("/ZPRJ_HOD", sSelectedValue);
+			this.getView().getModel("oModelTempValue").setProperty("/HODName", hodName);
+		},
+		// resp
+		// on Value Help(F4)
+		onresValueHelp: function () {
+			if (!this.resFrag) {
+				this.resFrag = sap.ui.xmlfragment(
+					"pgp.com.budgetmaster.view.fragments.resF4",
+					this
+				);
+				this.getView().addDependent(this.resFrag);
+				this._resTemp = sap.ui
+					.getCore()
+					.byId("idSLresValueHelp")
+					.clone();
+			}
+
+			var aFilter = [];
+			sap.ui.getCore().byId("idSDresF4").bindAggregation("items", {
+				path: "/ZpsBudPerHodSet",
+				filters: aFilter,
+				template: this._resTemp,
+			});
+
+			this.resFrag.open();
+		},
+
+		// on Value Help - Search/liveChange
+		onValueHelpSearch_res: function (oEvent) {
+			var aFilter = [];
+			var sValue = oEvent.getParameter("value");
+			var sPath = "/ZpsBudPerHodSet";
+			var oSelectDialog = sap.ui.getCore().byId(oEvent.getParameter("id"));
+
+			var oFilterName = new Filter(
+				[new Filter("NameTextc", FilterOperator.Contains, sValue)],
+				false
+			);
+			aFilter.push(oFilterName);
+
+			oSelectDialog.bindAggregation("items", {
+				path: sPath,
+				filters: aFilter,
+				template: this.resFrag,
+			});
+		},
+
+		// on Value Help - Confirm
+		onValueHelpConfirm_res: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem"),
+				sSelectedValue = oSelectedItem.getProperty("title"),
+				resName = oSelectedItem.getProperty("description") + "-";
+			this
+				.getView()
+				.getModel("oModel")
+				.setProperty("/ZWBS_OWNER", sSelectedValue);
+			this.getView().getModel("oModelTempValue").setProperty("/ResName", resName)
+		},
 
 	});
 });
